1c06135fd10530da82d368789f42319a8ce08692
IvayloMutsov <145490251+IvayloMutsov@users.noreply.github.com>
2025-12-16 15:30:22 +0000

    .

--- zooShop/resources/views/admin/animal_types/edit.blade.php
+++ zooShop/resources/views/admin/animal_types/edit.blade.php
@@ -5,15 +5,15 @@
 @section('content')
 <h2>@yield('title')</h2>
 
-<form action="{{ isset($type) ? route('admin.animal-types.update', $type) : route('admin.animal-types.store') }}" method="POST">
+<form action="{{ isset($animalType) ? route('admin.animal-types.update', $animalType->id) : route('admin.animal-types.store') }}" method="POST">
     @csrf
-    @if(isset($type))
+    @if(isset($animalType))
         @method('PUT')
     @endif
 
     <div class="mb-3">
         <label>Name</label>
-        <input type="text" name="name" class="form-control" value="{{ $type->name ?? '' }}" required>
+        <input type="text" name="name" class="form-control" value="{{ $animalType->name ?? old('name') }}" required>
     </div>
 
     <button class="btn btn-success">Save</button>
